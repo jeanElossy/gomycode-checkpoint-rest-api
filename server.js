34dbdeda59948bb7d092c5a9f239bd9431d4db03
JeanElossy <jean.elossy@yahoo.com>
2021-10-25 09:38:35 +0100

    Initialisation du projet

--- server.js
+++ server.js
@@ -1,30 +1,97 @@
 //require('dotenv').config();
 const express = require('express');
+const { db } = require('./model/user');
 const app = express();
 require('./model/db-config');
 const PostModel = require('./model/user');
+const bodyParser = require('body-parser')
+
+// import de ObjectId pour faire le put
+const ObjectID = require('mongoose').Types.ObjectId;
+
+app.use('/', bodyParser.json());
 
 app.listen(8080, (err) => {
     !err ? console.log(`server starting`) : console.log(`error server: ${err}`)
 })
 
-
+//obtenir les donnees
 app.get('/', (req, res) => {
     //le header
     res.setHeader('Content-Type', 'text/html');
 
     //le corps
-    res.status(200).send(PostModel.find());
+    PostModel.find((err, docs) => {
+        if(!err) {
+            res.send(docs);
+        }else{
+            console.log(`error get: ${err}`)
+        }
+    })
 })
 
+// methode post
 app.post('/', (req, res) => {
+	const newUser = new PostModel({
+		nom  : req.body.nom,
+		prenom : req.body.prenom
+	});
 
-})
+	newUser.save((err, docs) => {
+		if (!err) {
+			res.send(docs);
+		}
+		else {
+			console.log(err);
+		}
+	});
+});
 
-app.put('/', (req, res) => {
+// methode put
+app.put('/:id', (req, res) => {
+	if (!ObjectID.isValid(req.params.id)) {
+		res.status(400).send(`ID pas connu: ${req.params.id}`);
+	}
+	else {
+		const udpdateUser = {
+			nom: req.body.nom,
+			prenom: req.body.prenom,
+		};
 
-})
+		PostModel.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set : udpdateUser,
+			},
+			{
+				new : true,
+			},
+			(err, docs) => {
+				if (!err) {
+					res.send(docs);
+				}
+				else {
+					console.log(`update error: ${err}`);
+				}
+			},
+		);
+	}
+});
 
-app.delete('/', (req, res) => {
+// methode delete
+app.delete('/:id', (req, res) => {
+	if (!ObjectID.isValid(req.params.id)) {
+		res.status(400).send('ID non connu : ' + err);
+	}
+	else {
+		PostModel.findOneAndRemove(req.params.id, (err, docs) => {
+			if (!err) {
+				res.send(docs);
+			}
+			else {
+				console.log(err);
+			}
+		});
+	}
+});
 
-}) 
